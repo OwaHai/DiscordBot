ba276e9014cc09bc2d08f7b4e419bf85394cb848
Owa_Beta <meero65@gmail.com>
2018-06-02 18:14:36 +0300

    Update install.bat

--- install.bat
+++ install.bat
@@ -5,7 +5,7 @@ goto installs
 cls
 
 :installs
-echo Please wait while we're installing NPM packages...
+echo Please wait while we're installing NPM packages... Keep in mind you MUST be in the folder your bot is in or else this wont work
 npm install discord.js
 pause
 goto done
