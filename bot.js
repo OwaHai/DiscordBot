e04742bf16e88579b395b609745af9af12a33254
Owa_Beta <meero65@gmail.com>
2018-06-02 17:32:38 +0300

    Created main file, 24 lines.

---
+++ bot.js
@@ -0,0 +1,24 @@
+const Discord = require('discord.js');
+const client = new Discord.Client();
+const config = require("./config.json");
+
+client.on('ready', () => {
+  console.log(`Ready.`);
+});
+
+const prefix = "%";
+
+client.on("message", (message) => {
+  if (!message.content.startsWith(prefix) || message.author.bot) return;
+  
+  if (message.content.startsWith(prefix + "ping")) {
+    message.channel.send("Pong! `" + client.ping + "`ms);
+  } else
+  if (message.content.startsWith(prefix + "hello")) {
+    message.channel.send({embed: {
+  color: 3447003,
+  title: "Hello, " + message.author +".,
+  }
+});
+
+client.login(config.token);
